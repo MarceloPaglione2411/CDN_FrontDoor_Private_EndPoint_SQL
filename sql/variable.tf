34502260e4ef95052b181bc57f6c71ca01ae65c8
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-25 18:29:35 -0300

    Update variable.tf

--- sql/variable.tf
+++ sql/variable.tf
@@ -1,1 +1,81 @@
+variable "rg2_name" {
+    description = "Nome RG NAME"
+    type = string
+}
+
+variable "rg2_location" {
+    description = "LOCATION RG NAME"
+    type = string
+}
+
+variable "sql_server_name" {
+    description = "Nome SQL SERVER NAME"
+    type = string
+}
+
+variable "sql_server_version" {
+    description = "Nome SQL SERVER VERSION"
+    type = string
+}
+
+variable "login_mssql-server" {
+    description = "LOGIN SQL SERVER"
+    type = string
+}
+
+variable "password_mssql-server" {
+    description = "PASSWORD SQL SERVER"
+    type = string
+}
+
+variable "sql_database_name1" {
+    description = "DATABASE NAME"
+    type = string
+}
+
+variable "vnet_prvendpoint_name" {
+    description = "NOME VNET PRIVATEENDPOINT"
+    type = string
+}
+
+variable "snet_vms_name" {
+    description = "NOME sNET VMS"
+    type = string
+}
+
+variable "snet_vms_name_id" {
+    description = "id sNET VMS"
+    type = string
+}
+
+variable "vnet_prvendpoint_id" {
+    description = "ID VNET PRIVATEENDPOINT"
+    type = string
+}
+
+variable "snet_prvendpoint_name" {
+    description = "nome snet prvendpoint"
+    type = string
+}
+
+variable "snet_prvendpoint_id" {
+    description = "id snet prvendpoint"
+    type = string
+}
+
+variable "private_endpoint_name" {
+    description = "PRIVATE ENDPOINT NAME"
+    type = string
+}
+
+variable "dns_zone_name" {
+    description = "NOME DO DOMINIO - DNS"
+    type = string
+}
+
+variable "dns_zone_name_id" {
+    description = "ID NOME DO DOMINIO - DNS"
+    type = string
+}
+
 
