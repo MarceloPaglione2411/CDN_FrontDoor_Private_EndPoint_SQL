47104039ff4ea2404421cbb1247613baca6980e8
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-25 18:27:44 -0300

    Update variable.tf

--- resource_group/variable.tf
+++ resource_group/variable.tf
@@ -1,1 +1,9 @@
+variable "rg2_name" {
+    description = "Nome Resource Group (HVNETrg2)"
+    type = string
+}
 
+variable "rg2_location" {
+    description = "Localização RG2"
+    type = string
+}
