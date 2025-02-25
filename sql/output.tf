a28506de9ec4e61fd11343e2352f17cf581a0340
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-25 18:29:17 -0300

    Update output.tf

--- sql/output.tf
+++ sql/output.tf
@@ -1,1 +1,40 @@
+output "virtual_network_name_prvendpoint" {
+  description = "NOME DA VNET DO PRIVATE ENDPOINT."
+  value       = azurerm_virtual_network.mod-vnet-prvendpoint.name
+}
+
+output "virtual_network_prvendpoint_id" {
+  description = "ID DA VNET DO PRIVATE ENDPOINT."
+  value       = azurerm_virtual_network.mod-vnet-prvendpoint.id
+}
+
+output "subnet_name_prvendpoint" {
+  description = "NOME DA SNET DO PRIVATE ENDPOINT"
+  value       = azurerm_subnet.mod-snet-prvendpoint.name
+}
+
+output "subnet_name_vms" {
+  description = "NOME DA SNET DAS VMS"
+  value       = azurerm_subnet.mod-snet-vms.name
+}
+
+output "subnet_name_vms_id" {
+  description = "NOME id SNET DAS VMS"
+  value       = azurerm_subnet.mod-snet-vms.id
+}
+
+output "subnet_name_prvendpoint_id" {
+  description = "id DA SNET DO PRIVATE ENDPOINT"
+  value       = azurerm_subnet.mod-snet-prvendpoint.id
+}
+
+output "private_endpoint_name" {
+  description = "NOME DO PRIVATE ENDPOINT"
+  value       = azurerm_private_endpoint.mod_private_endpoint.name
+}
+
+output "private_endpoint_id" {
+  description = "ID DO PRIVATE ENDPOINT"
+  value       = azurerm_private_endpoint.mod_private_endpoint.id
+}
 
