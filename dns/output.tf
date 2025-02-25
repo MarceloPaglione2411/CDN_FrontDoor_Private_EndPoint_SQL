d4a36d8bc026887e2825a167a08a98564e60b159
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-02-25 18:25:33 -0300

    Update output.tf

--- dns/output.tf
+++ dns/output.tf
@@ -1,1 +1,11 @@
+output "dns_zone_name" {
+    value = azurerm_private_dns_zone.mod-dns-zone.name
+}
 
+output "dns_zone_name_id" {
+    value = azurerm_private_dns_zone.mod-dns-zone.id
+}
+
+output "dns_network_link_name" {
+    value = azurerm_private_dns_zone_virtual_network_link.mod-dns-network-link.name
+}
